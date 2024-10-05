28e367473dfe8c2bfbe24b9935c5a95b97beace1
Lucas Matricarde <lucas.eduardo@wises.com.br>
2024-10-05 13:43:03 -0300

    Criando a entity MarcaProduto

---
+++ src/main/java/lucasmatricar/com/lojajava/model/MarcaProduto.java
@@ -0,0 +1,31 @@
+package lucasmatricar.com.lojajava.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+    private String nomeDesc;
+
+    public MarcaProduto() {
+    }
+
+    public MarcaProduto(Long id, String nomeDesc) {
+        this.id = id;
+        this.nomeDesc = nomeDesc;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNomeDesc() {
+        return nomeDesc;
+    }
+}
