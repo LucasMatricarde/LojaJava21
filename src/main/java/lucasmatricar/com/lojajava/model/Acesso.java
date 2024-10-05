e3e70e58d76b0f2242f5b752b4608c7ac6d443db
Lucas Matricarde <lucas.eduardo@wises.com.br>
2024-10-05 18:51:30 -0300

    Implementação classes: Acesso,Endereco, Pessoa, PessoaFisica, PessoaJuridica

---
+++ src/main/java/lucasmatricar/com/lojajava/model/Acesso.java
@@ -0,0 +1,48 @@
+package lucasmatricar.com.lojajava.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", allocationSize = 1, initialValue = 1)
+public class Acesso implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao; /*Acesso ex: ROLE_ADMIN ou ROLE_SECRETARIO*/
+
+    public Acesso(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Acesso acesso = (Acesso) o;
+        return Objects.equals(id, acesso.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/lucasmatricar/com/lojajava/model/Endereco.java
@@ -0,0 +1,107 @@
+package lucasmatricar.com.lojajava.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "endereco")
+@SequenceGenerator(name = "seq_endereco", sequenceName = "seq_endereco", allocationSize = 1, initialValue = 1)
+public class Endereco implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_endereco")
+    private Long id;
+
+    private String logradouro;
+
+    private String numero;
+
+    private String complemento;
+
+    private String cep;
+
+    private String bairro;
+
+    private String cidade;
+
+    private String uf;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,name = "fk_endereco_pessoa"))
+    private Pessoa pessoa;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+}
