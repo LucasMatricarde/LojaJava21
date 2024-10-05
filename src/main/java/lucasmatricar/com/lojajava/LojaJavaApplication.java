92f2d731b68b0109897a36f71ea86e89d31abd6a
Lucas Matricarde <lucas.eduardo@wises.com.br>
2024-10-05 13:08:51 -0300

    Começando projeto

---
+++ src/main/java/lucasmatricar/com/lojajava/LojaJavaApplication.java
@@ -0,0 +1,13 @@
+package lucasmatricar.com.lojajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucasmatricar/com/lojajava/LojaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package lucasmatricar.com.lojajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
