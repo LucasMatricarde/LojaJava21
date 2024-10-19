9bf1480c20e4b78efcdb7427fdcd4a93f36fe311
Lucas Matricarde <lucas.eduardo@wises.com.br>
2024-10-19 19:15:28 -0300

    Teste mock

--- src/main/java/lucasmatricar/com/lojajava/LojaJavaApplication.java
+++ src/main/java/lucasmatricar/com/lojajava/LojaJavaApplication.java
@@ -2,6 +2,7 @@ package lucasmatricar.com.lojajava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @ComponentScan(basePackages = {"lucasmatricar.*"})
 @EnableJpaRepositories(basePackages = {"lucasmatricar.com.lojajava.repository"})
+@EntityScan(basePackages = "lucasmatricar.com.lojajava.model")
 @EnableTransactionManagement
 public class LojaJavaApplication {
 
--- src/test/java/lucasmatricar/com/lojajava/LojaJavaApplicationTests.java
+++ src/test/java/lucasmatricar/com/lojajava/LojaJavaApplicationTests.java
@@ -1,28 +1,50 @@
 package lucasmatricar.com.lojajava;
 
-import junit.framework.TestCase;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lucasmatricar.com.lojajava.controller.AcessoController;
 import lucasmatricar.com.lojajava.model.Acesso;
-import lucasmatricar.com.lojajava.repository.AcessoRepository;
-import lucasmatricar.com.lojajava.service.AcessoService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest(classes = LojaJavaApplication.class)
-public class LojaJavaApplicationTests extends TestCase {
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class LojaJavaApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Autowired
 	private AcessoController acessoController;
 
 	@Test
+	public void testRestApiCadastraAcesso() throws Exception {
+		Acesso acesso = new Acesso();
+		acesso.setDescricao("ROLE_COMPRADOR");
+
+		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/saveAcesso")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(acesso))
+				.accept(MediaType.APPLICATION_JSON));
+
+		resultActions.andExpect(status().isOk());
+	}
+
+	@Test
 	public void testeCadastraAcesso() {
 		Acesso acesso = new Acesso();
-
 		acesso.setDescricao("ROLE_ADMIN");
 
 		acesso = acessoController.saveAcesso(acesso).getBody();
 
-        assertTrue(acesso.getId() > 0);
+		assert acesso != null;
+		assert acesso.getId() > 0;
 	}
 }
